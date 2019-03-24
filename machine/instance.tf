2856be86150b01019216e128bd831b1126903262
Kirill Zonov <kzonov@babbel.com>
2019-03-24 12:24:47 +0100

    Added kops installation

--- machine/instance.tf
+++ machine/instance.tf
@@ -1,7 +1,13 @@
+data "template_file" "user_data" {
+  template = "${file("user_data.sh")}"
+}
+
 resource "aws_instance" "bastion" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name      = "kzonov"
+
+  user_data = "${data.template_file.user_data.rendered}"
 
   network_interface {
     network_interface_id = "${aws_network_interface.bastion.id}"
@@ -10,5 +16,6 @@ resource "aws_instance" "bastion" {
 
   tags {
     Owner = "kzonov"
+    Name  = "kzonov-temp"
   }
 }
--- machine/ssh.tf
+++ machine/ssh.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "kzonov" {
   key_name   = "kzonov"
-  public_key = "ssh-rsa mysshkey graffzon@gmail.com"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0mazLQqP7zScOz0/JjKrUS/TaBufxBFCwVs9+NgD1jltmat5pbeHD8+iv0Ks/EEkulotlGNnSa/fMyBxIef6PWKhaKzHjfQMUOllOOfJrRM6PPvtUSZ4mT9MCk914YUt7bYoRZAekaqvpSMSFcncL39QvCjyCtc24vPY9oJe5sDT8AGIA7m/PiHfd4Qq2C0qPz8QdHIEP25q9AL9eX+CUwCJIKQEtntnHMZZAP3dqG9QqCu7hk/65Yerc55TUIerrpGNMsS6cCwvn7+TfM8VNpzvrytLmTI5Eyeocc96CCv1xwxXC1glHF9E5X1ALc2feBeON+ei1iQbEhzVDsK1J graffzon@gmail.com"
 }
