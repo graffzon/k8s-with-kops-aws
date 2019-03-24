82a82068a51b6277d138ede67f7fd871120cb2c5
Kirill Zonov <kzonov@babbel.com>
2019-03-24 16:22:04 +0100

    Added s3 bucket to store the kops state

---
+++ k8s/s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "state-storage-s3" {
+  bucket = "temp-kubernetes-remote-state-storage-s3-kzonov"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
