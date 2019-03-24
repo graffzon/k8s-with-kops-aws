0b6ddf3c2ee1c610254ce4fd45492e26cf7fa234
Kirill Zonov <kzonov@babbel.com>
2019-03-24 15:50:48 +0100

    Removed master node creation and moved the kops back to the bastion

--- bastion/user_data.sh
+++ bastion/user_data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+echo "installing KOPS..."
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install -y apt-transport-https
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+echo "installing Kubectl..."
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
--- k8s/networking.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_network_interface" "master" {
-  subnet_id       = "${data.terraform_remote_state.network.private_2_subnet_id}"
-  security_groups = ["${aws_security_group.private.id}"]
-}
